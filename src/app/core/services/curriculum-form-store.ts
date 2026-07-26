dc66e0c6743c9215423c1942a03728e760436610
joschonarth <joschonarth@gmail.com>
2026-07-26 07:35:05 -0300

    feat: add CurriculumFormStore service with personal information form

---
+++ src/app/core/services/curriculum-form-store.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CurriculumFormStore {
+  private readonly curriculumForm = new FormGroup({
+    personal: new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      age: new FormControl(null, [Validators.required]),
+      address: new FormControl('', [Validators.required]),
+      state: new FormControl('', [Validators.required]),
+      city: new FormControl('', [Validators.required]),
+      maritalStatus: new FormControl('', [Validators.required]),
+    }),
+  });
+}
