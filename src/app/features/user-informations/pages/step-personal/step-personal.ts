cd1450ae098d2ca52ef15cb0484481421ebe98d9
joschonarth <joschonarth@gmail.com>
2026-07-26 08:02:55 -0300

    feat: update personal form submission to use button type and improve navigation

--- src/app/features/user-informations/pages/step-personal/step-personal.ts
+++ src/app/features/user-informations/pages/step-personal/step-personal.ts
@@ -1,13 +1,18 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { CurriculumFormStore } from '../../../../core/services/curriculum-form-store';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-step-personal',
-  imports: [RouterLink, ReactiveFormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './step-personal.html',
 })
 export class StepPersonal {
   readonly _curriculumFormStore = inject(CurriculumFormStore);
+  private readonly _router = inject(Router);
+
+  goToProfessional() {
+    this._router.navigate(['/professional']);
+  }
 }
