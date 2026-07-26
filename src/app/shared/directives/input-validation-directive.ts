7f7e84004096aca7338b12b7c637bf501ac537d8
joschonarth <joschonarth@gmail.com>
2026-07-26 16:45:16 -0300

    feat: enhance input validation directive

--- src/app/shared/directives/input-validation-directive.ts
+++ src/app/shared/directives/input-validation-directive.ts
@@ -1,13 +1,36 @@
-import { Directive, Input } from '@angular/core';
+import { DestroyRef, Directive, inject, Input } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { NgControl } from '@angular/forms';
 
 @Directive({
   selector: '[appInputValidation]',
   standalone: true,
 })
 export class InputValidationDirective {
+  private readonly _ngControl = inject(NgControl, { self: true });
+  private readonly _destroyRef = inject(DestroyRef);
+
   @Input() errorMessage = 'Campo inválido';
 
   ngOnInit() {
     console.log('OnInit errorMessage:', this.errorMessage);
+
+    this._ngControl.statusChanges?.pipe(takeUntilDestroyed(this._destroyRef)).subscribe(() => {
+      this.updateStatus();
+    });
+  }
+
+  private updateStatus() {
+    const control = this._ngControl.control;
+
+    if (!control) return;
+
+    const isInvalid = control.invalid && control.touched;
+
+    if (isInvalid) {
+      console.log('Está inválido');
+    } else {
+      console.log('Está válido');
+    }
   }
 }
