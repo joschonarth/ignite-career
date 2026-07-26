52a432933743a62dfcade1a567be95562f1ebdda
joschonarth <joschonarth@gmail.com>
2026-07-26 08:23:41 -0300

    feat: implement dynamic state selection in personal form

--- src/app/features/user-informations/pages/step-personal/step-personal.ts
+++ src/app/features/user-informations/pages/step-personal/step-personal.ts
@@ -1,7 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { CurriculumFormStore } from '../../../../core/services/curriculum-form-store';
 import { ReactiveFormsModule } from '@angular/forms';
+import { StatesAndCitiesApi } from '../../../../core/services/states-and-cities-api';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-step-personal',
@@ -11,6 +13,20 @@ import { ReactiveFormsModule } from '@angular/forms';
 export class StepPersonal {
   readonly _curriculumFormStore = inject(CurriculumFormStore);
   private readonly _router = inject(Router);
+  private readonly _statesAndCitiesApi = inject(StatesAndCitiesApi);
+
+  statesResource = rxResource({
+    params: () => true,
+    stream: () => this._statesAndCitiesApi.getStates(),
+  });
+
+  statesList = computed(() => {
+    const ERROR_ON_RESPONSE = !!this.statesResource.error();
+
+    if (ERROR_ON_RESPONSE) return [];
+
+    return this.statesResource.value();
+  });
 
   goToProfessional() {
     this._router.navigate(['/professional']);
